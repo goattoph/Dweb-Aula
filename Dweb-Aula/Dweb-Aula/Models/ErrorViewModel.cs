6b89b1f4532a8f0d935c86af9a24f9f532132f17
goattoph <ruiafonsopt02@gmail.com>
2024-03-13 10:46:52 +0000

    Código inicial do Projeto

---
+++ Dweb-Aula/Dweb-Aula/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dweb_Aula.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
